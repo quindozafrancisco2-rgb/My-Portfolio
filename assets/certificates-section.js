1d817c930c76dc6592bbc418d0d4595890600a3f
quindozafrancisco2-rgb <quindozafrancisco2@gmail.com>
2026-03-02 21:53:24 +0800

    Update certificates-section.js

--- assets/certificates-section.js
+++ assets/certificates-section.js
@@ -9,25 +9,25 @@
       title: "Intern of the Month",
       issuer: "Lucid Strategies LLC",
       year: "May 2025",
-      href: "./assets/images/intern-of-the-month.jpg"
+      href: "./images/intern-of-the-month.jpg"
     },
     {
       title: "Certificate of Internship",
       issuer: "Lucid Strategies LLC",
       year: "2025",
-      href: "./assets/images/certificate-of-internship.jpg"
+      href: "./images/certificate-of-internship.jpg"
     },
     {
       title: "ICpEP R4B CpExplore 2021 – Certificate of Participation",
       issuer: "Institute of Computer Engineers of the Philippines (ICpEP)",
       year: "2021",
-      href: "./assets/images/icpep-cpexplore-2021.jpg"
+      href: "./images/icpep-cpexplore-2021.jpg"
     },
     {
       title: "CERT Operations: Vulnerability Management",
       issuer: "Department of Information and Communications Technology (DICT)",
       year: "Nov 23, 2022",
-      href: "./assets/images/dict-cert-vulnerability-management-2022.jpg"
+      href: "./images/dict-cert-vulnerability-management-2022.jpg"
     }
   ];
 
@@ -416,3 +416,4 @@
     if (insertBeforeWork() || tries >= maxTries) clearInterval(timer);
   }, 300);
 })();
+
